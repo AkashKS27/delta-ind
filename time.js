ccfb027dda32b65f5f74009448bfe00bbe30bf3b
Akash.K.S <aakash2703@gmail.com>
2016-06-01 23:18:04 +0530

    Update time.js

--- time.js
+++ time.js
@@ -1,10 +1,10 @@
-var d, p;
+var d, p, q;
 var dead;
 var deadline;
-alert("press start to start the timer");
+alert("Press Start button to start the Timer");
 
 function start(display) {
-  dead = prompt("enter deadline date", "June 1,2016 00:00:00");
+  dead = prompt("Enter Deadline Date", "June 3,2016 00:00:00");
   deadline = new Date(dead);
 
   function timer() {
@@ -17,7 +17,7 @@ function start(display) {
     timeleft -= minu * 60000;
     var sec = parseInt(timeleft / 1000);
     if (sec >= 0 && minu >= 0 && hou >= 0 && day >= 0) {
-      display.textContent = "DAYS :" + day + ":" + "HOURS :" + hou + ":" + "MINUTES :" + minu + ":" + "SECONDS :" + sec;
+      display.textContent = "DAYS :" + day + "  HOURS :" + hou + "  MINUTES :" + minu + "  SECONDS :" + sec;
 
     }
     if (sec == 0 && minu == 0 && hou == 0 && day == 0) {
@@ -28,48 +28,49 @@ function start(display) {
   d = setInterval(timer, 1000);
 }
 
-function sta(display) {
-  deadline = new Date(dead);
+function sta(disp) {
 
   function timer() {
-    var timeleft = deadline - new Date();
-    var day = parseInt(timeleft / 86400000);
-    timeleft -= day * 86400000;
-    var hou = parseInt(timeleft / 3600000);
-    timeleft -= hou * 3600000;
-    var minu = parseInt(timeleft / 60000);
-    timeleft -= minu * 60000;
-    var sec = parseInt(timeleft / 1000);
-    if (sec >= 0 && minu >= 0 && hou >= 0 && day >= 0) {
-       display.textContent = "DAYS :" + day + ":" + "HOURS :" + hou + ":" + "MINUTES :" + minu + ":" + "SECONDS :" + sec;
+    var timelef = deadline - new Date();
+    var da = parseInt(timelef / 86400000);
+    timelef -= da * 86400000;
+    var ho = parseInt(timelef / 3600000);
+    timelef -= ho * 3600000;
+    var min = parseInt(timelef / 60000);
+    timelef -= min * 60000;
+    var se = parseInt(timelef / 1000);
+    if (se >= 0 && min >= 0 && ho >= 0 && da >= 0) {
+      display.textContent = "DAYS :" + da + "  HOURS :" + ho + "  MINUTES :" + min + "  SECONDS :" + se;
     }
-    if (sec == 0 && minu == 0 && hou == 0 && day == 0) {
+    if (se == 0 && min == 0 && ho == 0 && da == 0) {
       alert("LIFT OFF");
     }
   };
   timer();
-  d = setInterval(timer, 1000);
+  q = setInterval(timer, 1000);
 }
 
 function star() {
 
-  display = document.getElementById('time');
+  display = document.getElementById("time");
   start(display);
 }
 
 function stop() {
   clearInterval(d);
+  clearInterval(q);
+  q = null;
   d = null;
   document.getElementById("time").innerHTML = 0;
 }
 
 function pause() {
   clearInterval(d);
-  p = new Date();
+  clearInterval(q);
 }
 
 function cont() {
-  display = document.getElementById('time');
-  sta(display);
+  disp = document.getElementById("time");
+  sta(disp);
 
 }
